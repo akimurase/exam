5109cad9b411da28bb46859081aff48b144c0ce1
akimurase <mosworldfor@gmail.com>
2021-02-18 16:12:57 +0900

    ログインサインインページ作成 #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # devise_for :admins
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  # root to: ''
 end

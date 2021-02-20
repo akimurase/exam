23cbf0860680c5d1c19a309d3369e5c8b8d04f68
akimurase <mosworldfor@gmail.com>
2021-02-20 14:44:37 +0900

    管理者機能実装 #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # devise_for :admins
-  devise_for :admins, controllers: {
-    sessions:      'admins/sessions',
-    passwords:     'admins/passwords',
-    registrations: 'admins/registrations'
-  }
-  # root to: ''
+  devise_for :admins
+  get 'tests/index'
+  # devise_for :admins, controllers: {
+  #   sessions:      'admins/sessions',
+  #   passwords:     'admins/passwords',
+  #   registrations: 'admins/registrations'
+  # }
+  # root to: 'dmins#sessions'
 end

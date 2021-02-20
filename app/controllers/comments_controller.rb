1ce74e630cfd9ab47965025c3311db82f1ff0f9e
akimurase <mosworldfor@gmail.com>
2021-02-20 18:22:37 +0900

    編集削除機能実装 #2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
   def index
     @comment = Comment.new
     @comments = Comment.all
+    # @comment_delete = Comment.find(params[:id])
+    # @comment_edit = Comment.find(comment_params_edit)
   end
 
   def create
@@ -16,14 +18,23 @@ class CommentsController < ApplicationController
   end
 
   def update
+    comment = Comment.find(params[:id])
+    comment.update(comment_params_edit)
+    redirect_to root_path
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to root_path
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:text)#.merge(user_id: current_user.id)
+  end
+  def comment_params_edit
+    params.permit(:text)#.merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  # get 'comments/index'
-  # get 'comments/new'
-  # get 'comments/create'
-  # get 'comments/show'
-  # get 'comments/edit'
-  # get 'comments/update'
-  # get 'comments/destroy'
-
   root to: 'comments#index'
   resources :comments
 end
